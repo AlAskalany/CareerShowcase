668331f83d03096bead00d60e2780b8dc0ce6a30
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-11-10 12:46:24 +0100

    Converted ExampleInstrumentedTest to Kotlin

--- app/src/androidTest/java/com/alaskalany/careershowcase/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/alaskalany/careershowcase/ExampleInstrumentedTest.kt
@@ -22,29 +22,29 @@
  * SOFTWARE.
  */
 
-package com.alaskalany.careershowcase;
+package com.alaskalany.careershowcase
 
-import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import android.content.Context
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert.assertEquals
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
 
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        val appContext = InstrumentationRegistry.getTargetContext()
 
-        assertEquals("com.alaskalany.careershowcase", appContext.getPackageName());
+        assertEquals("com.alaskalany.careershowcase", appContext.packageName)
     }
 }
