6d1522660f5469e8056e4739f4b765f8b1e4e883
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-11-18 10:49:50 +0100

    Inlined NetworkHandler in MainActivity

--- app/src/main/java/com/alaskalany/careershowcase/MainActivity.java
+++ app/src/main/java/com/alaskalany/careershowcase/MainActivity.java
@@ -24,8 +24,13 @@
 
 package com.alaskalany.careershowcase;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import com.alaskalany.careershowcase.databinding.ActivityMainBinding;
@@ -94,7 +99,90 @@ public class MainActivity
         bottomNavigationManager.init(savedInstanceState == null);
         
         // Handle connectivity
-        NetworkHandler networkHandler = new NetworkHandler(getApplicationContext());
+        android.content.Context context1 = getApplicationContext();
+        Runnable networkHandler = new Runnable() {
+            
+            private final Context context = context1;
+            
+            /**
+             * When an object implementing interface <code>Runnable</code> is used
+             * to create a thread, starting the thread causes the object's
+             * <code>run</code> method to be called in that separately executing
+             * thread.
+             * <p>
+             * The general contract of the method <code>run</code> is that it may
+             * take any action whatsoever.
+             *
+             * @see Thread#run()
+             */
+            @Override
+            public void run() {
+                // Get the connectivity manager
+                ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
+                
+                boolean isWifiConn = false;
+                boolean isMobileConn = false;
+                
+                // Get active network information
+                NetworkInfo networkInfo1 = connMgr.getActiveNetworkInfo();
+                
+                // Online if network info exists and is connected
+                boolean isOnline = networkInfo1 != null && networkInfo1.isConnected();
+                
+                if (isOnline) {
+                    // If online
+                    // Check connectivity for all networks
+                    for (Network network : connMgr.getAllNetworks()) {
+                        NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
+                        if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                            // If Wi-Fi
+                            isWifiConn |= networkInfo.isConnected();
+                            if (isWifiConn) {
+                                doWhenWifiIsConnected();
+                            }
+                        }
+                        if (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                            // If mobile network
+                            isMobileConn |= networkInfo.isConnected();
+                            if (isMobileConn) {
+                                doWhenMobileIsConnected();
+                            }
+                        }
+                    }
+                } else {
+                    // If not connected
+                    doWhenNotConnected();
+                }
+            }
+            
+            /**
+             * If connected through Wi-Fi do this
+             */
+            private void doWhenWifiIsConnected() {
+                // TODO handle if Wi-Fi is connected
+                Toast.makeText(context.getApplicationContext(), "WiFi connected", Toast.LENGTH_SHORT)
+                     .show();
+            }
+            
+            /**
+             * If connected through mobile network do this
+             */
+            private void doWhenMobileIsConnected() {
+                // TODO handle if mobile network is connected
+                Toast.makeText(context.getApplicationContext(), "Mobile connected", Toast.LENGTH_SHORT)
+                     .show();
+            }
+            
+            /**
+             * If not connected do this
+             */
+            private void doWhenNotConnected() {
+                
+                // TODO handle if disconnected
+                Toast.makeText(context.getApplicationContext(), "Not connected", Toast.LENGTH_SHORT)
+                     .show();
+            }
+        };
         networkHandler.run();
     }
     
